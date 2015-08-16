75a441f62623923256e22849318e4624aeabf406
cmh553 <cmh553@nau.edu>
2015-08-15 18:20:02 -0700

    Working Angular. Major styling changes.
    -Wallpaper is full background for the whole site. Dynamically changes when adjusting the window size, and does not display on mobile screen sizes.
    -Added a white rounded box attribute to contain all body content for each page, so the wallpaper background does not get in the way.
    -The test page is the temporary page for the step by step data upload process. All done in angular with ui-router.
    -Test page is not complete. Problem with two-way data binding on the validation page.
    -Other minor fixes.

--- website/public/js/ngForm.js
+++ website/public/js/ngForm.js
@@ -8,19 +8,27 @@ form.config(function($stateProvider, $urlRouterProvider){
         .state('form', {
             url: '/form',
             templateUrl: 'views/form.html',
-            controller: 'FormCtrl',
+            controller: 'FormCtrl'
         })
         .state('form.upload', {
             url: '/upload',
-            templateUrl: 'views/form-upload.html',
+            templateUrl: 'views/form-upload.html'
         })
-        .state('form.contents', {
-            url: '/contents',
-            templateUrl: 'views/form-contents.html'
+        .state('form.content', {
+            url: '/content',
+            templateUrl: 'views/form-content.html'
+        })
+        .state('form.validate', {
+            url: '/validate',
+            templateUrl: 'views/form-validate.html'
         })
         .state('form.success', {
             url: '/success',
             templateUrl: 'views/form-success.html'
+        })
+        .state('form.thanks', {
+            url: '/thanks',
+            templateUrl: 'views/form-thanks.html'
         });
 
         $urlRouterProvider.otherwise('/form/upload');
@@ -28,8 +36,9 @@ form.config(function($stateProvider, $urlRouterProvider){
 
 form.controller('FormCtrl', function($scope){
     $scope.formData = {};
+    $scope.userData = {};
     $scope.processForm = function(){
-        alert('alert!');
+        alert('Sent to Server');
     };
     $scope.showContent = function($fileContent){
         $scope.formData = $fileContent;
--- website_old/geo/static/js/login.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by chrisheiser1 on 6/12/15.
- */
