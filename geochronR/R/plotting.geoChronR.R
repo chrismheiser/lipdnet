6f82407fc481004d465e32a3bf3482376814949a
nickmckay <nick@mckays.us>
2016-08-08 17:12:08 -0700

    summary plots

--- geochronR/R/plotting.geoChronR.R
+++ geochronR/R/plotting.geoChronR.R
@@ -1,11 +1,102 @@
-map.lipd = function(L){
-  
+map.lipd = function(L,color="red",size=8,shape = 16,zoom=4){
+ library(ggmap)
+  dfp = data.frame(lon = L$geo$longitude,lat = L$geo$latitude)
+  basemap = get_googlemap(center = c(dfp$lon,dfp$lat),zoom=zoom)
+  map = ggmap(ggmap = basemap)  + geom_point(data=dfp,aes(x=lon,y=lat),colour = color,size=size,shape = shape) 
+  if(!is.null( L$geo$siteName)){
+    dfp$sitename = L$geo$siteName
+    map=map+geom_label(data=dfp,aes(x=lon,y=lat,label=sitename),nudge_y=-1)
+  }
+  return(map)
 }
+
+map.lipds = function(D,color= sapply(D,"[[","archiveType"),size=8,shape = 16 ){
+  library(ggmap)
+  dfp = data.frame(lon = sapply(D,function(x) x$geo$longitude),lat = sapply(D,function(x) x$geo$latitude),color,shape)
+  dfp = dfp[!is.na(dfp$lat) & !is.na(dfp$lon),]
+  rLat=range(dfp$lat)
+  rLon=range(dfp$lon)
+  cent=c(mean(rLon),mean(rLat))
+  zoom = calc_zoom(rLon,rLat)
+  basemap = get_googlemap(center = cent,zoom=zoom,source = "stamen")
+
+  
+
+  map = ggmap(ggmap = basemap)  + geom_point(data=dfp,aes(x=lon,y=lat,colour = color),shape = shape,size=7)
+  return(map)
+}
+
 
 #show a map, timeseries, and age model diagram..
 summary.plot = function(L){
+  map = map.lipd(L)
+  
+  #plot paleoData
+  
+  print("What should we plot on the X-axis?")
+  print("We'll look for age or year...")
+  age=select.data(L,varName = "age",always.choose = FALSE,altNames="year")
+  
+  print("What should we plot on the Y-axis?")
+  variable=select.data(L)
+  
+  paleoPlot = plot.line(age,variable)
+  paleoPlot = paleoPlot + labs(title = paste("PaleoData:",variable$variableName))
+  
+  #do chron.
+  if(any(names(L)=="chronData")){
+    #looking for age model data.
+    
+    print("looking for ages...")
+    age2=select.data(L,varName = "age14C",always.choose = FALSE,altNames="year",where = "chronData")
+    
+#     print("looking for age uncertainty")
+#     ageUnc = select.data(L,varName = "age14CUncertainty",always.choose = FALSE,altNames=c("uncertainty","error","age"),where = "chronData")
+    
+    print("looking for depths....")
+    depth=select.data(L,varName = "depth",always.choose = FALSE,where = "chronData")
+    c.df = data.frame(x=age2$values,y=depth$values)
+   chronPlot = plot.line(age2,depth)
+   chronPlot = chronPlot+
+     geom_point(data=c.df,aes(x=x,y=y),colour="black",size=7)+
+     scale_y_reverse()+
+     scale_x_reverse()+
+     labs(title = paste("ChronData: chronology"))
+    
+   lay = rbind(c(1,1,2,2),
+               c(3,3,2,2),
+               c(3,3,4,4),
+               c(3,3,4,4))
+
+   
+   dataSetText = paste(L$dataSetName,"\n","Archive Type: ",L$archiveType,"\n","Publication Metadata: ",L$pub)
+   summaryText = grobTree(rectGrob(gp = gpar(fill = 1,alpha=.1)), textGrob(dataSetText) )
+   
+   summary = grid.arrange(grobs = list(summaryText,paleoPlot,map,chronPlot),layout_matrix=lay)    
+   
+    
+  }
+  
+  
+  
   
 }
+
+#X and Y and are LiPD variables, including values, units, names, etc...
+plot.line = function(X,Y,color="black",add.to.plot=ggplot()){
+  df = data.frame(x = X$values, y = Y$values)
+  plot = add.to.plot+ geom_line(data=df,aes(x=x,y=y),colour =color)+
+  ylab(paste0(Y$variableName," (",Y$units,")"))+
+    xlab(paste0(X$variableName," (",X$units,")"))+
+    theme_bw()
+  if(tolower(X$variableName)=="age"){
+    plot = plot+scale_x_reverse()
+  }
+  return(plot)
+  
+  
+}
+
 
 
 plot.timeseries.lines = function(X,Y,alp=.2,color = "blue",maxPlotN=1000,add.to.plot=ggplot()){
