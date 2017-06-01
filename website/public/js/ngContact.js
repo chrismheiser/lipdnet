c568f096a11f712a05cfdb225973f8fc72719095
CHRISTOPHER HEISER <heiser@nau.edu>
2017-06-01 13:42:05 -0700

    Safeguards for spam bots on the contact form
    - We’ve been getting some spam bots in the contact form sending empty
    messages. (Harmless but annoying). Added some safeguards to hopefully
    keep them from being able to submit an empty form, or use a randomly
    generated hash as a Name.

--- website/public/js/ngContact.js
+++ website/public/js/ngContact.js
@@ -1,11 +1,49 @@
 var c = angular.module('ngContact', ['ngMaterial', 'ngAnimate', 'ngColors']);
 
 
-c.controller('ContactCtrl', ['$scope', '$http', function($scope, $http){
+c.controller('ContactCtrl', ['$scope', '$http', "$window", function($scope, $http, $window){
 
-  $scope.sent = 'false';
+  $scope.fmsnt = false;
+  $scope.complete = false;
+  // using generic vars to avoid spam bots sending messages on the form. 
+  $scope.fm = {"fn": "", "fo": "", "fs": "", "fmm": "", "fe": ""};
+  $scope.checkForm = function(){
+  	// check if the keys are empty or filled. 
+  	for (var key in $scope.fm){
+  		if ($scope.fm.hasOwnProperty(key)){
+  			var i = $scope.fm.key;
+  			if (!i || i === " " || i === "" || i === undefined){
+  				$scope.complete = false;
+  				return;
+  			}
+  		}
+  	}
+  	$scope.complete = true;
+  	return;
+  };
+
+  $scope.checkInput = function(t){
+  	var trigger = false;
+  	if (t === "fn"){
+	  	if (!(!/[^a-z]/i.test($scope.fm.fn))){
+	  		$scope.fm.fn = "";
+	  		trigger = true;
+	  	}
+  	} else if (t === "fo"){
+	  	if (!(!/[^a-z]/i.test($scope.fm.fo))){
+	  		$scope.fm.fo = "";
+	  		trigger = true;
+	  	}
+  	}
+  	if(trigger){
+  		$window.alert("Numbers and special characters not allowed in this field")
+  		$scope.complete = false;
+  	}
+  	return;
+  }
+
   $scope.sendMail = function(m){
-    $scope.sent = 'true'
+    $scope.sent = true;
     // $http.post('/', m).success(
     //   $scope.sent = true
     // );
